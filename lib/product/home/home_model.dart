a552347617a934afffe2274e38bd58e81ddcca6f
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-05-12 15:47:17 +0300

    Update home_model.dart

--- lib/product/home/home_model.dart
+++ lib/product/home/home_model.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 
 mixin HomeModel{
   File? selectedImage;
-  String? message="";
-  final url = 'http://10.0.2.2:5000/datas'; 
-  Uint8List? image;
+  final url = 'http://10.0.2.2:5000/process'; 
+  final saveurl = 'http://10.0.2.2:5000/save'; 
+  Uint8List? myimage;  
 }
